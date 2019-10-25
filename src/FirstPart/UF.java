591584a5e40c7eeedfd81799137fbcd504948837
Chedro Cardenas <chegiorca@hotmail.com>
2019-10-25 17:35:51 -0600

    Creating an Array and printing on the screen

--- src/FirstPart/UF.java
+++ src/FirstPart/UF.java
@@ -1,10 +1,18 @@
 package FirstPart;
 
+import java.util.Arrays;
+
 public class UF {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int data[] = new int [10];
+		for(int i = 0; i < 10;i++)
+		{
+			data[i]=i*1;
+			System.out.println(i);
+		}
+		System.out.println("Hello World " + Arrays.toString(data));
+		}
 	}
-
-}
